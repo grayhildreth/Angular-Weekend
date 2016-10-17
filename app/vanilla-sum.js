fa1fea02c7ba6fdd2ddfaece50d53e6cff8d771c
Gray Hildreth <grayhildreth@gmail.com>
2016-10-16 23:16:54 -0700

    Added code to weekend homework

---
+++ app/vanilla-sum.js
@@ -0,0 +1,5 @@
+
+var number = prompt('please enter a number.');
+var otherNumber = prompt('enter another number.');
+var sum = number + otherNumber;
+alert("The sum of your two numbers is: " + sum);
